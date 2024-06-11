bffee66d5626d7ece3024b7c6d5672d497ee9dd2
Nithin U J <girishnithin616@gmail.com>
2024-06-11 15:39:06 +0530

    q1.py

--- Python/q1.py
+++ Python/q1.py
@@ -1,21 +1,21 @@
 class Car:
-    def _init_(self, name, color, engine_hp):
+    def __init__(self, name, color, engine_hp):
         self.name = name
         self.color = color
         self.engine_hp = engine_hp
 
-def drive(self):
-    print("{self.name} Car is driving")
+    def drive(self):
+        print(f"{self.name} Car is driving")
 
 class FlyingCar(Car):
-    def _init_(self, name, color, engine_hp):
-        super()._init_(name, color, engine_hp)
+    def __init__(self, name, color, engine_hp):
+        super().__init__(name, color, engine_hp)
 
     def fly(self):
-        print("{self.name} Car is flying")
+        print(f"{self.name} Car is flying")
 
-if name == "main":
-    fc = FlyCar("SuperFly", "Black", 5000)
+if __name__ == "__main__":
+    fc = FlyingCar("SuperFly", "Black", 5000)
     fc.drive()
     fc.fly()
 
