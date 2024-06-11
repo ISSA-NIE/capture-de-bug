5f2caac51e4718e6fa3947ebdfbfc87f564bb262
Nithin U J <girishnithin616@gmail.com>
2024-06-11 15:36:28 +0530

    q1.java

--- Java/q1.java
+++ Java/q1.java
@@ -1,47 +1,34 @@
 import java.lang.String;
-
 class Car {
-    String name;
-    String color;
-    int engineHP;
-
-    Car() {
-        System.out.print(name);
-    }
-
-    Car(String name, String color, int engineHP) {
-        this.name = name;
-        this.color = color;
-        this.engineHP = engineHP;
-    }
-
-    private void drive() {
-        System.out.print(name + " Car is driving");
-    }
+String name;
+String color;
+int engineHP;
+Car() {
 }
-
+Car(String name, String color, int engineHP) {
+this.name = name;
+this.color = color;
+this.engineHP = engineHP;
+}
+void drive() {
+System.out.println(name + " Car is driving");
+}
+}
 class FlyingCar extends Car {
-    FlyingCar() {}
-
-    FlyingCar(String name, String color, int engineHP) {
-        this.name = name;
-        this.color = color;
-        this.engineHP = engineHP;
-    }
-
-    void fly() {
-        System.out.print(name + " Car is flying");
-    }
+FlyingCar() {}
+FlyingCar(String name, String color, int engineHP) {
+this.name = name;
+this.color = color;
+this.engineHP = engineHP;
 }
-
+void fly() {
+System.out.println(name + " Car is flying");
+}
+}
 public class Main {
-    public static void main(String[] args) {
-        FlyCar fc = new FlyingCar("SuperFly", "Black", 5000);
-        fc.drive();
-        fc.fly();
-    }
+public static void main(String[] args) {
+FlyingCar fc = new FlyingCar("SuperFly", "Black", 5000);
+fc.drive();
+fc.fly();
 }
-
-// Expected Output:
-// SuperFly Car is driving
-// SuperFly Car is flying
+}
